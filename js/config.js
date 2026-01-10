9ce4d441dece24a0fe0b5995b03ed6aa884400f6
rusherdhasu <rusherdhasu@gmail.com>
2026-01-10 13:29:52 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = 'https://fuisdgf4416515uisgduifDHASUAUTHihoiggdfg48g4dg.onrender.com';
+const API_BASE_URL = 'https://authblackholebackend.onrender.com';
 window.API_BASE_URL = API_BASE_URL;
